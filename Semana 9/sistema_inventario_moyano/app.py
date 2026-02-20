aca5c4439a4c6ac98f14054b2a44439edd403ddd
Dario2025 <da.moyanom@uea.edu.ec>
2026-02-20 10:33:21 -0500

    Comint

--- Semana 9/sistema_inventario_moyano/app.py
+++ Semana 9/sistema_inventario_moyano/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -15,4 +16,23 @@ def productos():
     return render_template("productos.html")
 
 if __name__ == '__main__':
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/productos')
+def productos():
+    return render_template("productos.html")
+
+if __name__ == '__main__':
+>>>>>>> e06ce9decf0e60957145b20ceabd82e594b1ec01
     app.run(host="0.0.0.0", port=5000)
